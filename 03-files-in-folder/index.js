95512335ff2036b0ea3a015c9794f34313752038
Denis Tutov <vrednokot@gmail.com>
2023-05-05 16:34:55 +0300

    fix: add checking files for type

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,21 @@
 const { readdir, stat } = require('fs/promises');
-const { join, extname } = require('path');
+const { join } = require('path');
 
 const FOLDER = join(__dirname, 'secret-folder');
 
 readdir(FOLDER, { withFileTypes: true })
   .then(dir => {
     dir.forEach(el => {
-      stat(join(FOLDER, el.name))
-        .then(file => {
-          const [fileName, fileExt] = [...el.name.split('.')];
-          const fileSize = file.size > 1024 ?
-           `${(file.size / 1024).toFixed(3)} 'kb'`:
-           `${file.size} 'b'`;
+      if(!el.isDirectory()) {
+        stat(join(FOLDER, el.name))
+          .then(file => {
+            const [fileName, fileExt] = [...el.name.split('.')];
+            const fileSize = file.size > 1024 ?
+             `${(file.size / 1024).toFixed(3)}kb`:
+             `${file.size}b`;
 
-          console.log(`${fileName} - ${fileExt} - ${fileSize}`);
-        }).catch(err => console.log(err));
+            console.log(`${fileName} - ${fileExt} - ${fileSize}`);
+          }).catch(err => console.log(err));
+      }
     })
   });
